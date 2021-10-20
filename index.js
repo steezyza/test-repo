fb8be95870fe9b2027c0233d5ae79addb9088e0f
L Kenqa <lskenqa@gmail.com>
2021-10-20 17:02:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = "Linda";
+let age = 28;
+
+if name == "Linda" && age == 28 {
+    console.log("IKDR");
+} else if name == "Siya" {
+    console.log("That ain't your name broski");
+} else {
+    console.log("Also not your name");
+}
+
+const turtles = ["Raphael","Michelangel","Leonardo","Donatello"]
+
+function turtlePower (turtles) {
+    return turtles[3];
+}
